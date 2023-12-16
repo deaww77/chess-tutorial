13b01f730eef3813cb360e777b1e3ee5abb15e36
chanon roopkhum <126137481+deaww77@users.noreply.github.com>
2023-12-16 20:18:23 +0700

    chess tutorial

---
+++ javascript2/script.js
@@ -0,0 +1,7 @@
+console.log('Hello World!');
+const deaw = {
+    fisrtname: "chanon",
+    lastname: "roopkhum"
+};
+console.log(deaw);
+//document.getElementById("demo").innerHTML = "gorwnjer9jekfjdsolkfjpdofjsaqlferognjcnklwef000000000000000000000000000000000000000000000rkonmht";
