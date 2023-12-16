29d626d8dce53eeb553f7d2922415ee5a099dd6b
chanon roopkhum <126137481+deaww77@users.noreply.github.com>
2023-12-16 23:09:07 +0700

    Delete javascript2 directory

--- javascript2/script.js
+++
@@ -1,7 +0,0 @@
-console.log('Hello World!');
-const deaw = {
-    fisrtname: "chanon",
-    lastname: "roopkhum"
-};
-console.log(deaw);
-//document.getElementById("demo").innerHTML = "gorwnjer9jekfjdsolkfjpdofjsaqlferognjcnklwef000000000000000000000000000000000000000000000rkonmht";
